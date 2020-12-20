6963719dfb73e339795464b0e14d4f3b1d853fa9
Yushachkov Anton <71340057+Pro100kak2s@users.noreply.github.com>
2020-12-20 19:55:29 +0300

    Add files via upload

---
+++ MovablePoint.java
@@ -0,0 +1,36 @@
+package ANDREY;
+
+public class MovablePoint implements Movable {
+    int x;
+    int y;
+    int xSpeed;
+    int ySpeed;
+
+    public MovablePoint() {
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" + "x=" + x + ", y=" + y + ", xSpeed=" + xSpeed + ", ySpeed=" + ySpeed + '}';
+    }
+
+    @Override
+    public void moveUP() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+}
